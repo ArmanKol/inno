25cb7bd867c977e0549fac0e1aa25dcee60b3071
ArmanKol <arman.koldaguc@gmai.com>
2019-12-18 23:46:53 +0100

    fix

--- src/inno/Main.java
+++ src/inno/Main.java
@@ -1,13 +1,22 @@
 package inno;
 
+import java.util.concurrent.TimeUnit;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		Item items = new Item();
 		
-		for(Item item : items.getAllItems("EnergyConsumption", true)) {
-			Thread mt = new Thread(new MultiThreading(item, item.getName()));
-			mt.start();
+		while(true) {
+			for(Item item : items.getAllItems("EnergyConsumption", true)) {
+				if(Integer.parseInt(item.getState()) < 5) {
+					System.out.println("AAAAAAAAA");
+				}else {
+					System.out.println(item.getName() + ":" + item.getState());
+				}
+				
+			}
+			TimeUnit.SECONDS.sleep(5);
 		}
 
 		//Gui gui = new Gui();
--- src/inno/MultiThreading.java
+++ src/inno/MultiThreading.java
@@ -1,58 +1,19 @@
 package inno;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.concurrent.TimeUnit;
-
-import org.json.JSONObject;
-
 public class MultiThreading implements Runnable{
 	private Item item;
-	private String itemName;
 	
-	public MultiThreading(Item item, String itemName) {
+	public MultiThreading(Item item) {
 		this.item = item;
-		this.itemName = itemName;
 	}
-	
-	//Integer.parseInt(item.getState()) < 5
 	
 	public void run() {
-	    while(true) {
-	    	BufferedReader bufferedReader;
-			String inputLine;
-	    	try {
-	        	item.setConnection(item.initURLConnection(itemName, "GET", true));
-    			
-    			bufferedReader = new BufferedReader(
-    					  new InputStreamReader(item.getConnection().getInputStream()));
-    			final StringBuffer content = new StringBuffer();
-    			
-    			while ((inputLine = bufferedReader.readLine()) != null) {
-    				content.append(inputLine);
-    			}
-    			bufferedReader.close();
-    			
-    			//Parse to json
-    			final JSONObject json = new JSONObject(content.toString());
-    			item.setState(json.getString("state"));
+    	if(Integer.parseInt(item.getState()) < 5) {
+    		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+    	}else {
+    		System.out.println(item.getName() + ":" + item.getState());
+    	}
 
-	        	if(Integer.parseInt(item.getState()) < 5) {
-	        		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-	        		Thread.sleep(5000);
-	        	}else {
-	        		System.out.println(item.getName() + ":" + item.getState());
-					Thread.sleep(5000);
-	        	}
-
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			} catch (IOException io) {
-				io.printStackTrace();
-			}
-	        
-	    }
-	}
+	} 
 
 }
